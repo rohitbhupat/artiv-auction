37ad513e1eb4315b6cef3ffec7f36e0b5ca7f58d
rohitbhupat <bhupatrohit90@gmail.com>
2025-03-20 18:00:00 +0530

    add allowed host

--- art_auction/art_auction/settings.py
+++ art_auction/art_auction/settings.py
@@ -162,7 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
